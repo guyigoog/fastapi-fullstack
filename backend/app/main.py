f9347dc23ba1034afd10368300736143773ae680
Guy Perry <guyigoog@gmail.com>
2024-06-03 23:26:13 +0300

    cors?

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,16 +20,18 @@ app = FastAPI(
     generate_unique_id_function=custom_generate_unique_id,
 )
 
+origins = [
+    "http://localhost","http://localhost:5173","https://localhost","https://localhost:5173","http://localhost.tiangolo.com"
+]
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
-        ],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
+        expose_headers=["*"],
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
